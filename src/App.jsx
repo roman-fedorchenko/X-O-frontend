731b28680be690f2f99e7fe5e970cde4a5cd48d9
Fedorchenko Roman <dgbzznbx@gmail.com>
2026-02-11 11:06:46 +0200

    3

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,11 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        hello world
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>This is test page for vercel</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
