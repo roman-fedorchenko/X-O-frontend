be79f6259f0d0728bbaf1bf893cae55fd98414a6
Fedorchenko Roman <dgbzznbx@gmail.com>
2026-02-11 13:07:25 +0200

    5

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>This is test page for vercel</h1>
+      <h1>Abra cadabra</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
